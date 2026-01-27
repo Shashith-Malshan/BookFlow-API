8c9c58379c75de90c0f7de39f97bce0a1a347087
Shashith-Malshan <malshanshashith318@gmail.com>
2026-01-27 18:22:38 +0530

    Implement DTO class PlaceOrderRequestDTO

--- src/main/java/model/dto/PlaceOrderRequestDTO.java
+++ src/main/java/model/dto/PlaceOrderRequestDTO.java
@@ -4,10 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import model.entity.OrderDetails;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class PlaceOrderRequestDTO {
+    private Date date;
+    private Long customerId;
+
+    private ArrayList<OrderDetails> orderDetails=new ArrayList<>();
 }
