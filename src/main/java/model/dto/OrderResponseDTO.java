bcba4ae9c8c6f6d6911aa7118c81fb448222f47c
Shashith-Malshan <malshanshashith318@gmail.com>
2026-01-27 18:21:50 +0530

    Implement DTO class OrderResponseDTO

--- src/main/java/model/dto/OrderResponseDTO.java
+++ src/main/java/model/dto/OrderResponseDTO.java
@@ -4,10 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import model.entity.OrderDetails;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderResponseDTO {
+    private Long orderId;
+    private Date date;
+    private Long customerId;
+
+    private ArrayList<OrderDetails> orderDetails=new ArrayList<>();
+
+
 }
