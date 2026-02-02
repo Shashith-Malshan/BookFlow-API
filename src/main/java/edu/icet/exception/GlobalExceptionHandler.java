0810a9ef24e19d9c08249d45898f4494c8df3538
Shashith-Malshan <malshanshashith318@gmail.com>
2026-02-02 21:37:56 +0530

    Add GlobalExceptionHandler

---
+++ src/main/java/edu/icet/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package edu.icet.exception;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(InsufficientStockException.class)
+    public Map<String, Object> handleInsufficientStock(InsufficientStockException ex) {
+
+        Map<String, Object> error = new HashMap<>();
+        error.put("timestamp", LocalDateTime.now());
+        error.put("status", 400);
+        error.put("error", "Bad Request");
+        error.put("message", ex.getMessage());
+
+        return error;
+    }
+}
